170f3ab4e79af0635b3542c0a2cb7d9d6e1ac5d1
IMWoo94 <leemeawoo94@gmail.com>
2023-10-24 19:15:50 +0900

    spring data jpa jpa and spring data jpa simple apply and test 2023.10.24

---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package study.datajpa.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import study.datajpa.domain.Member;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Test
+	void testMember() {
+		Member member = new Member("memberA");
+		Member savedMember = memberRepository.save(member);
+
+		Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+		assertThat(findMember.getId()).isEqualTo(member.getId());
+		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+		assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장
+	}
+
+}
