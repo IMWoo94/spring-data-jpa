945e36fbb34a3f0bdff5a0928e9db8c659e22721
IMWoo94 <leemeawoo94@gmail.com>
2023-10-25 16:18:26 +0900

    spring data jpa query method funtion simple apply 2023.10.25

--- data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,10 +2,9 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "study.datajpa.repository")
+// @EnableJpaRepositories(basePackages = "study.datajpa.repository")
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -2,6 +2,11 @@ package study.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +20,8 @@ class MemberRepositoryTest {
 
 	@Autowired
 	MemberRepository memberRepository;
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void testMember() {
@@ -28,6 +35,56 @@ class MemberRepositoryTest {
 		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 
 		assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장
+	}
+
+	@Test
+	void basicCRUD() {
+		Member member1 = new Member("member1");
+		Member member2 = new Member("member2");
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		// 단건 조회 검증
+		Member findMember1 = memberRepository.findById(member1.getId()).get();
+		Member findMember2 = memberRepository.findById(member2.getId()).get();
+		assertThat(findMember1).isEqualTo(member1); // JPA 엔티티 동일성 보장
+		assertThat(findMember2).isEqualTo(member2); // JPA 엔티티 동일성 보장
+
+		// 리스트 조회 검증
+		List<Member> all = memberRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+
+		// 카운트 검증
+		long count1 = memberRepository.count();
+		assertThat(count1).isEqualTo(2);
+
+		// 삭제 검증
+		memberRepository.delete(member1);
+		memberRepository.delete(member2);
+
+		// 카운트 검증
+		long count2 = memberRepository.count();
+		assertThat(count2).isEqualTo(0);
+	}
+
+	@Test
+	void queryMethodFnTest() {
+		Member member = new Member("query1");
+		memberRepository.save(member);
+
+		em.flush();
+		em.clear();
+
+		Member referenceById = memberRepository.getReferenceById(member.getId());
+		System.out.println("referenceById.getClass() = " + referenceById.getClass());
+
+		em.flush();
+		em.clear();
+
+		List<Member> byUsername = memberRepository.findByUsername(member.getUsername());
+		for (Member member1 : byUsername) {
+			System.out.println("member1.getUsername() = " + member1.getUsername());
+		}
 	}
 
 }
