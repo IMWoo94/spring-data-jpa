db95d5c20e0d7e303ab11be85680230cc6a981c5
IMWoo94 <leemeawoo94@gmail.com>
2023-10-29 18:16:07 +0900

    spring data jpa auditing all apply orm.xml 2023.10.29

--- data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
+++ data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
@@ -1,20 +1,18 @@
 package study.datajpa.entity;
 
 import javax.persistence.Column;
-import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
 
 /**
  * auditing 을 적용하고 싶다면, 꼭 listeners 를 등록해주어야 한다.
- * 전체에 적용하고 싶다면, 별도의 xml 에 등록 하고 전체적으로 등록이 되도록 선언하자.
+ * 전체에 적용하고 싶다면, 별도의 orm.xml 에 등록 하고 전체적으로 등록이 되도록 선언하자.
  */
-@EntityListeners(AuditingEntityListener.class)
+// @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseEntity extends BaseTimeEntity {
--- data-jpa/src/main/java/study/datajpa/entity/BaseTimeEntity.java
+++ data-jpa/src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -3,16 +3,14 @@ package study.datajpa.entity;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
-import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
 
-@EntityListeners(AuditingEntityListener.class)
+// @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseTimeEntity {
